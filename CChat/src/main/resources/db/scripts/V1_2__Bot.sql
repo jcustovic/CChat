32f43489e79ad5f7b45d113f0bdcac9d55690bec
chusica <chusica@47cfbc82-8faa-46f4-9396-a362a7c9afc5>
2012-12-15 14:11:40 +0000

    - explicit engine and charset in sql scripts

--- CChat/src/main/resources/db/scripts/V1_2__Bot.sql
+++ CChat/src/main/resources/db/scripts/V1_2__Bot.sql
@@ -5,11 +5,13 @@ CREATE TABLE bot (
 	, online BOOL NOT NULL DEFAULT true
 	, PRIMARY KEY(id)
 	, UNIQUE (name)
-);
+)
+ENGINE=InnoDB
+CHARSET=UTF8;
 
 ALTER TABLE `users` ADD COLUMN `bot_id` INTEGER UNSIGNED DEFAULT NULL;
 ALTER TABLE `users` ADD CONSTRAINT `bot_id_fk` FOREIGN KEY(`bot_id`) REFERENCES `bot`(`id`);
 
 ALTER TABLE `sms_message` ADD COLUMN `bot_response` BOOL NOT NULL DEFAULT false;
 
-INSERT INTO `bot`(name, description, online) VALUES ("SimpleBot", "First simple bot", true)
+INSERT INTO `bot`(name, description, online) VALUES ("SimpleBot", "First simple bot", true);
