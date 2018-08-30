25272815735c15efc9b3ef114b6a3ce60ed98e11
jan.custovic@gmail.com <jan.custovic@gmail.com>
2018-08-30 14:23:20 +0200

    Added missing bean config

--- CChat/src/main/java/hr/chus/cchat/struts2/action/admin/TalkToBot.java
+++ CChat/src/main/java/hr/chus/cchat/struts2/action/admin/TalkToBot.java
@@ -1,33 +1,30 @@
 package hr.chus.cchat.struts2.action.admin;
 
+import com.opensymphony.xwork2.ActionSupport;
 import hr.chus.cchat.helper.UserAware;
 import hr.chus.cchat.model.db.jpa.Operator;
-
 import org.aitools.programd.Core;
 import org.aitools.programd.bot.Bot;
 import org.aitools.programd.util.DeveloperError;
 import org.aitools.programd.util.XMLKit;
 import org.springframework.beans.factory.annotation.Autowired;
 
-import com.opensymphony.xwork2.ActionSupport;
-
 /**
  * @author Jan Čustović (jan.custovic@gmail.com)
  */
-@SuppressWarnings("serial")
 public class TalkToBot extends ActionSupport implements UserAware {
 
     @Autowired
     private transient Core core;
 
-    private Operator       currentUser;
+    private Operator currentUser;
 
-    private String         message;
-    private String         botId;
-    private String         response;
+    private String message;
+    private String botId;
+    private String response;
 
     @Override
-    public final String execute() throws Exception {
+    public final String execute() {
         try {
             final Bot bot = core.getBot(botId);
             response = core.getResponse(message, currentUser.getUsername(), bot.getID());
